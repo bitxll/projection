d030ebf65118f91777c51fa393dcdbe013cc5e40
xllbit <1304@bit.edu.cn>
2017-08-18 14:47:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,17 +8,14 @@ import pprint
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '2'
 flags = tf.app.flags
-flags.DEFINE_integer("epoch", 2000, "Number of epoch [10]")
-flags.DEFINE_integer("batch_size", 16, "The size of batch images [128]")
-flags.DEFINE_integer("n_inputs", 128, "The size of image to use [33]")
-flags.DEFINE_integer("n_outputs", 128, "The size of image to use [33]")
-flags.DEFINE_integer("out_step", 4, "The size of image to use [33]")
-flags.DEFINE_integer("n_steps", 64, "The size of label to produce [21]")
-flags.DEFINE_integer("n_hidden_units", 128, "The size of label to produce [21]")
+flags.DEFINE_integer("epoch", 2000, "Number of epoch [2000]")
+flags.DEFINE_integer("batch_size", 16, "The size of batch for each inputs [64]")
+flags.DEFINE_integer("n_inputs", 128, "The size of input [128]")
+flags.DEFINE_integer("n_outputs", 128, "The size of output [128]")
+flags.DEFINE_integer("out_step", 20, "The size of step for output [20]")
+flags.DEFINE_integer("n_steps", 64, "The size of step for input [64]")
+flags.DEFINE_integer("n_hidden_units", 128, "The size of hidden units for LSTMCell [128]")
 flags.DEFINE_float("learning_rate", 1e-4, "The learning rate of gradient descent algorithm [1e-4]")
-flags.DEFINE_integer("c_dim", 1, "Dimension of image color. [1]")
-flags.DEFINE_integer("scale", 3, "The size of scale factor for preprocessing input image [3]")
-flags.DEFINE_integer("stride", 14, "The size of stride to apply input image [14]")
 flags.DEFINE_string("checkpoint_dir", "checkpoint", "Name of checkpoint directory [checkpoint]")
 flags.DEFINE_string("sample_dir", "sample", "Name of sample directory [sample]")
 flags.DEFINE_boolean("is_train", True, "True for training, False for testing [True]")
@@ -42,6 +39,7 @@ def main(_):
                   n_steps=FLAGS.n_steps,
                   out_step=FLAGS.out_step,
                   n_hidden_units=FLAGS.n_hidden_units,
+                  is_train =FLAGS.is_train,
                   checkpoint_dir=FLAGS.checkpoint_dir,
                   sample_dir=FLAGS.sample_dir)
 
